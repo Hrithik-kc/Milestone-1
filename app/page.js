732769ce1d5a7ccadff664bc5bc8a8935d9dd748
Hrithik-kc <hrithikkc1234@gmail.com>
2025-10-30 20:17:18 +0530

    second commit

--- app/page.js
+++ app/page.js
@@ -1,65 +1,239 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      <div>
+        <div className="bg-gray-900 h-13 w-full justify-between flex flex-row  ">
+          <div className="text-3xl text-amber-500 font-bold m-2">
+            Sahyadri Market
+          </div>
+          <button className="bg-green-600 h-8 w-24 rounded-md m-3.5">
+            Buy now
+          </button>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div>
+          <img
+            src="https://cdn.pixabay.com/video/2024/02/25/201947-916877801_tiny.jpg"
+            className="h-140 w-screen"
+          ></img>
+          <div className="flex flex-row justify-around -mt-136 font-semibold ">
+            <div className="flex flex-row">
+              <img
+                src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F6.png&w=64&q=75"
+                className="h-6 mx-1.5 w-auto"
+              ></img>
+              Phone:+919876534567
+            </div>
+            <div className="flex flex-row">
+              <img
+                src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F6.png&w=64&q=75"
+                className="h-6 mx-1.5 w-auto"
+              ></img>
+              Email:hrithikkc@gmail.com
+            </div>
+            <div>Location:Sahyadri Campuse</div>
+          </div>
+          <div className="mb-6">
+            ________________________________________________________________________________________________________________________________________________________
+          </div>
+          <div className="flex flex-row m-4 justify-around font-bold ">
+            <div>
+              <button className="mx-9">Home</button>
+              <button className="mx-9">Pages</button>
+              <button className="mx-9">Menu</button>
+            </div>
+            <img
+              src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Flogo-light.png&w=750&q=75"
+              className=" h-15 w-auto"
+            ></img>
+            <div>
+              <button className="mx-9">Blogs</button>
+              <button className="mx-9">Shop</button>
+              <button className="mx-9">Contact Us</button>
+            </div>
+          </div>
+          <div className="text-8xl font-semibold justify-self-center">
+            Best Restaurant
+          </div>
         </div>
-      </main>
+        <div className="bg-amber-600 h-2 w-screen mt-76"></div>
+      </div>
+      <div className="bg-gray-100 w-screen">
+        <div className=" flex flex-row ">
+          <div className="bg-white h-125 w-96 ml-32 rounded-3xl mt-10 shadow-md">
+            <div className="bg-amber-700 h-20 w-20 rounded-full ml-11 -mt-8"></div>
+            <div className="text-3xl text-black font-semibold ml-12 mt-10">
+              Book a Table
+            </div>
+            <input
+              className="h-12 w-72 ml-12 rounded-lg border pl-4 mt-7 text-gray-600 border-gray-400"
+              placeholder="Phone"
+            ></input>
+            <input
+              className="h-12 w-72 ml-12 rounded-lg border pl-4 mt-7 text-gray-600 border-gray-400"
+              placeholder="Members"
+            ></input>
+            <input
+              className="h-12 w-72 ml-12 rounded-lg border pl-4 mt-7 text-gray-600 border-gray-400"
+              placeholder="Date"
+            ></input>
+            <input
+              className="h-12 w-72 ml-12 rounded-lg border pl-4 mt-7 text-gray-600 border-gray-400"
+              placeholder="Time"
+            ></input>
+            <button className="h-11 w-34 ml-26 mt-4 rounded-lg bg-amber-900 text-xl font-medium">
+              Book a Table
+            </button>
+          </div>
+          <div className="flex flex-col">
+            <div className="text-4xl font-semibold text-black flex flex-row ml-32 mt-40">
+              <img
+                src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fshape%2Ffire.png&w=48&q=75"
+                className="h-12 w-auto"
+              ></img>
+              Our Popular Category
+              <div className=" flex flex-row gap-8 ml-16">
+                <div className="bg-amber-800 h-10 w-10 rounded-full gap-8"></div>
+                <div className="bg-amber-800 h-10 w-10 rounded-full"></div>
+              </div>
+            </div>
+            <div className="flex flex-row">
+              <div className="bg-black h-60 w-50 rounded-2xl ml-32 mt-8">
+                <img
+                  src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fmenu%2F25.jpg&w=828&q=75"
+                  className="h-60 w-auto rounded-2xl"
+                ></img>
+              </div>
+              <div className="bg-black h-60 w-50 rounded-2xl ml-8 mt-8">
+                <img
+                  src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fmenu%2F27.jpg&w=828&q=75"
+                  className="h-60 w-auto rounded-2xl"
+                ></img>
+              </div>
+              <div className="bg-black h-60 w-50 rounded-2xl ml-8 mt-8">
+                <img
+                  src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fmenu%2F27.jpg&w=828&q=75"
+                  className="h-60 w-auto rounded-2xl"
+                ></img>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-row  mt-32 justify-around m-16">
+          <div className="flex flex-col">
+            <img
+              src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F13.png&w=256&q=75"
+              className="h-30 w-30"
+            ></img>
+            <div className="text-2xl font-semibold text-black mt-5">
+              Quality Foods
+            </div>
+            <div className="text-gray-500 mt-5">
+              Belonging sir curiosity discovery{" "}
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              extremity yet forfeited prevailed own
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              {" "}
+              off. Traveling by introduced of mr
+            </div>
+            <div className="text-gray-500 mt-2.5">terminated.</div>
+          </div>
+          <div className="flex flex-col">
+            <img
+              src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F14.png&w=256&q=75"
+              className="h-30 w-30"
+            ></img>
+            <div className="text-2xl font-semibold text-black mt-5">
+              Fast Delivery
+            </div>
+            <div className="text-gray-500 mt-5">
+              Belonging sir curiosity discovery{" "}
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              extremity yet forfeited prevailed own
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              {" "}
+              off. Traveling by introduced of mr
+            </div>
+            <div className="text-gray-500 mt-2.5">terminated.</div>
+          </div>
+          <div className="flex flex-col">
+            <img
+              src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Ficon%2F15.png&w=256&q=75"
+              className="h-30 w-30"
+            ></img>
+            <div className="text-2xl font-semibold text-black mt-5">
+              Delicious Recipies
+            </div>
+            <div className="text-gray-500 mt-5">
+              Belonging sir curiosity discovery{" "}
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              extremity yet forfeited prevailed own
+            </div>
+            <div className="text-gray-500 mt-2.5">
+              {" "}
+              off. Traveling by introduced of mr
+            </div>
+            <div className="text-gray-500 mt-2.5">terminated.</div>
+          </div>
+        </div>
+        <div className="bg-amber-600 h-160 w-280 mt-32 justify-self-center rounded-4xl flex flex-row">
+          {/* <img src="https://png.pngtree.com/thumb_back/fh260/background/20220705/pngtree-decent-color-brush-background-image_1416414.jpg" className="h-158 w-277 justify- rounded-4xl ml-2 pt-2"></img> */}
+
+          <div>
+            <div className="flex flex-row">
+              <div>
+                <img
+                  src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fillustration%2F1.png&w=640&q=75"
+                  className="h-80 w-80 ml-11 mt-4"
+                ></img>
+              </div>
+              <div>
+                <img src="https://t4.ftcdn.net/jpg/03/36/91/17/360_F_336911714_WdVI3MgN9AhyL2nvW2ZxwkYCXwDABWfY.jpg"className="h-32 w-32 rounded-full mt-13 ml-24 "></img>
+              </div>
+            </div>
+            <div>
+              <img
+                src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fillustration%2F15.png&w=640&q=75"
+                className="h-70 w-70 ml-11"
+              ></img>
+            </div>
+            <div>
+              <img
+                src="https://restan-nextjs.vercel.app/_next/image?url=%2Fassets%2Fimg%2Fillustration%2F12.png&w=640&q=75"
+                className="h-60 w-60 ml-88 -mt-90"
+              ></img>
+            </div>
+          </div>
+          <div className="flex flex-col ml-14">
+            <div className="text-3xl font-bold text-black mt-24">
+              Today Special Offer
+            </div>
+            <div className="text-6xl font-semibold text-black mt-14">
+              Explore
+              <div>Irresistible</div>
+              <div>Promotions!</div>
+            </div>
+            <div className="mt-8 text-amber-950 font-medium">
+              <div>
+                Contrasted dissimilar get joy you instrument out reasonably.
+              </div>
+              <div>to zealously necessary.Again keeps at no meant stuff.</div>
+              <div>
+                northward as difficult preserved daughters. Continued at up
+              </div>
+              <div>To perpetual do existence</div>
+            </div>
+            <button className="bg-red-950 rounded-full h-13 hover:bg-black center mt-6 w-36 font-semibold">
+              Order Now
+            </button>
+          </div>
+        </div>
+        <div className="bg-amber-400 h-11 w-2xl mt-36"></div>
+      </div>
     </div>
   );
 }
